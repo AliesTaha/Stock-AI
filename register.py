ebfe1f59e5aab9aa7b26a0f9b1b324231a047eeb
Christian Aiello <93229991+christian-aiello@users.noreply.github.com>
2024-02-21 22:14:28 -0500

    Add argparse to registration script

--- register.py
+++ register.py
@@ -1,6 +1,14 @@
+import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import random, string
+import random, string, time
+
+parser = argparse.ArgumentParser(description="Automate registration on Investopedia Simulator")
+parser.add_argument("--email", required=True, help="Email address for registration")
+parser.add_argument("--username", required=True, help="Username for registration")
+parser.add_argument("--password", required=True, help="Password for registration")
+
+args = parser.parse_args()
 
 BASE_URL = 'https://www.investopedia.com/simulator/'
 
@@ -13,14 +21,16 @@ driver.maximize_window()
 driver.get(BASE_URL)
 login_button = driver.find_element(By.CSS_SELECTOR, "button[data-cy='landing-page-hero-registration-button']")
 login_button.click()
+
 email_input = driver.find_element(By.ID, "email")
 username_input = driver.find_element(By.ID, "username")
 password_input = driver.find_element(By.ID, "password")
 password_confirm_input = driver.find_element(By.ID, "password-confirm")
 submit_button = driver.find_element(By.ID, "register")
 
-email_input.send_keys("")
-username_input.send_keys("" + ''.join(random.choices(string.ascii_letters + string.digits, k=8)))
-password_input.send_keys("")
-password_confirm_input.send_keys("")
+email_input.send_keys(args.email)
+username_input.send_keys(args.username + "".join(random.choices(string.ascii_letters + string.digits, k=8)))
+password_input.send_keys(args.password)
+password_confirm_input.send_keys(args.password)
+time.sleep(random.random())
 submit_button.click()
